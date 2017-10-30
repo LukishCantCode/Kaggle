feca30fba394effdb574f4573a67ae60933f59e6
Lukish Yadav <yadavlukish@gmail.com>
2017-10-30 12:39:47 +0530

    Post deletion Commit

--- Dataframe_Normalization.py
+++ Dataframe_Normalization.py
@@ -2,27 +2,28 @@ import sklearn
 import pandas as pd
 import numpy as np
 
-#my_array = np.array([[1,2,3], [5,6,7]], dtype=np.int64)
+my_array = np.array([[1,2,3], [5,6,7]], dtype=np.int64)
 
-#data=pd.DataFrame(my_array,columns=['A','B','C'])
+data=pd.DataFrame(my_array,columns=['A','B','C'])
 
 
 import pandas as pd
 
-data = [{"low": 0, "high": 10, "name": "a"},
+data1 = [{"low": 0, "high": 10, "name": "a"},
         {"low": 10, "high": 20, "name": "b"},
         {"low": 20, "high": 30, "name": "c"},
         {"low": 30, "high": 40, "name": "d"},
         {"low": 40, "high": 50, "name": "e"},]
 
-myDF = pd.DataFrame(data)
+myDF = pd.DataFrame(data1)
 
 #data to be binned
-mySeries = pd.Series([5.7, 30.4, 21, 35.1])
+#mySeries = pd.Series([5.7, 30.4, 21, 35.1])
+Data=data['A']
 
 #create bins from original data
 bins = list(myDF["high"])
 bins.insert(0,0)
 
-print pd.cut(mySeries, bins, labels = myDF["name"])
+print pd.cut(Data, bins, labels = myDF["name"])
 
